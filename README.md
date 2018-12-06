cea8b2dd212b9fb71805da74b3c6fce25212f87e
Fred Qi <fred.qi@ieee.org>
2018-12-06 10:18:47 +0800

    Added README and config script.

---
+++ README.md
@@ -0,0 +1,10 @@
+# My Emacs Configuration
+
+To use this package, clone this repo to the path `~/.emacs.d/` and run the bash script `config.sh`. In a bash terminal, do this:
+```bash
+cd ~/.emacs.d
+git clone git@github.com:fredqi/site-lisp.git
+bash site-lisp/config.sh
+```
+
+Done.
---
+++ config.sh
@@ -0,0 +1,18 @@
+#/usr/bin/env bash
+
+sudo apt install -qq -y ttf-bitstream-vera
+
+PKGS="yaml-mode htmlize yasnippet cython-mode auctex android-mode header2"
+
+emacs -Q --batch --eval "(progn (require 'package)\
+  (setq package--init-file-ensured t)\
+  (package-initialize)\
+  (setq package-archives\
+        '((\"gnu\" . \"https://elpa.gnu.org/packages/\")\
+          (\"melpa\" . \"https://melpa.org/packages/\")
+          (\"marmalade\" . \"https://marmalade-repo.org/packages/\")))\
+  (package-refresh-contents)\
+  (dolist (pkg '($PKGS))\
+          (package-install pkg)))"
+
+ln -s site-lisp/init.el .
